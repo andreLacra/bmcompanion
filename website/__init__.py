520962ea7c9abbf31ed6284e6dd882b8f9e224fd
AndrePogi <lacraandre111@gmail.com>
2023-09-07 22:27:41 +0800

    tweaked db uri

--- website/__init__.py
+++ website/__init__.py
@@ -13,7 +13,7 @@ def create_app():
     app.config['SECRET_KEY'] = 'bUs33M4tch1ng*&^'
     # app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
     # app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:Andyzxc4@localhost/{DB_NAME_MYSQL}"
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://d9f4an67tbvdty5a:mw8b32khieu2rbqn@uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/jclcy9mxcyj918li"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://kpbkzjmq6gq90n32:bpohbsypqrpm3y20@uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ra8syicuxt9p3bqe"
     db.init_app(app)
 
 
